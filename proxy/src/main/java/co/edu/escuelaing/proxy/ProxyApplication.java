bfa1ce5e60bba47cf13b55081a864171623c7829
Maria Paula <maria.rmunoz@labinfo.is.escuelaing.edu.co>
2026-03-24 18:41:23 -0500

    docs: update readme

---
+++ proxy/src/main/java/co/edu/escuelaing/proxy/ProxyApplication.java
@@ -0,0 +1,11 @@
+package co.edu.escuelaing.math_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MathServiceApplication.class, args);
+	}
+}

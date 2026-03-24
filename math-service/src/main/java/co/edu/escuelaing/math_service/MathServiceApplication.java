7dfe7d9eeaeddf37ac2b69d0dceada38d4be49aa
unknown <maria.rmunoz@labinfo.is.escuelaing.edu.co>
2026-03-24 17:05:33 -0500

    first commit

---
+++ math-service/src/main/java/co/edu/escuelaing/math_service/MathServiceApplication.java
@@ -0,0 +1,11 @@
+package co.edu.escuelaing.math_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MathServiceApplication.class, args);
+	}
+}
---
+++ math-service/src/main/java/co/edu/escuelaing/math_service/controllers/MathController.java
@@ -0,0 +1,22 @@
+package co.edu.eci.lambda.springrest;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+private static final String template = "Hello, %s!";
+private final AtomicLong counter = new AtomicLong();
+
+@GetMapping("/greeting")
+public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+}
+
+@RequestParam
+
+}
